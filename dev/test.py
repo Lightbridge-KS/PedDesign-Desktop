09ad7c8d5d33e09112b9e3f6dc92abacc4b506c1
Kittipos <ki11ip0.s.a.s@gmail.com>
2024-01-13 09:49:10 +0700

    update test

--- dev/test.py
+++ dev/test.py
@@ -1,9 +1,8 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(1, str(Path(".").absolute()))
+sys.path.insert(1, str(Path.cwd()))
+# print(sys.path)
 import peddesign  # noqa: E402
-# print(Path(".").absolute()) # Project Root
-
 
 peddesign.design_ct("whole_abd", 10)
