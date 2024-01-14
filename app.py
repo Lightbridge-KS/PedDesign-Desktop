ceff486d0b95773772f04cf81fdf6da537f74d8e
Kittipos <ki11ip0.s.a.s@gmail.com>
2024-01-14 18:13:57 +0700

    Implement `rate_formula` and `delay_time`

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import tkinter
 import customtkinter  
 import peddesign  
 from module import *  
+from helper import attempt_float
 # from helper import Capturing  
 
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
@@ -26,10 +27,20 @@ study_type_dict = {
 
 is_first_study_dict = {"Not First Study": False, "First Study": True}
 
+rate_formula_dict = {
+    "Fixed": "no_delay",
+    "Choose delayed time": "delay"
+}
+
+rate_formula_state = {
+    "Fixed": "disabled",
+    "Choose delayed time": "normal"
+}
+
 class MyInputFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
-        self.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
         # Logo Label
         self.logo_label = customtkinter.CTkLabel(
             self,
@@ -40,12 +51,11 @@ class MyInputFrame(customtkinter.CTkFrame):
 
         # Study Type
         self.optionmenu_study_type = MyOptionMenu(
-            self, values=list(study_type_dict.keys()), label_text="Study type:"
+            self, values=list(study_type_dict.keys()), label_text="Study type:", command=self.optionmenu_study_type
         )
         self.optionmenu_study_type.grid(
             row=1, column=0, padx=10, pady=(0, 0), sticky="ew"
         )
-        # self.optionmenu_study_type.configure(fg_color="transparent")
         # Age Group
         self.optionmenu_is_first_study = MyOptionMenu(
             self, values=list(is_first_study_dict.keys()), label_text="Occasion:"
@@ -53,29 +63,63 @@ class MyInputFrame(customtkinter.CTkFrame):
         self.optionmenu_is_first_study.grid(
             row=2, column=0, padx=10, pady=(0, 0), sticky="ew"
         )
-        # self.optionmenu_is_first_study.configure(fg_color="transparent")
         # Weight (kg)
         self.entry_weight_kg = MyEntry(
             self, placeholder_text="kg", label_text="Weight (kg):"
         )
         self.entry_weight_kg.grid(row=3, column=0, padx=10, pady=(0, 0), sticky="ew")
-        # self.entry_weight_kg.configure(fg_color="transparent")
-
+        # rate_formula (for `whole_abd` or `chest_whole_abd`)
+        self.optionmenu_rate_formula = MyOptionMenu(
+            self, state="disabled", values=list(rate_formula_dict.keys()), 
+            label_text="Rate calculation:", command=self.optionmenu_rate_formula_event
+        )
+        self.optionmenu_rate_formula.grid(
+            row=4, column=0, padx=10, pady=(0, 0), sticky="ew"
+        )
+        # Delayed Time (sec)
+        self.entry_delay_sec = MyEntry(
+            self, state="disabled", placeholder_text="sec", label_text="Delayed time (sec):"
+        )
+        self.entry_delay_sec.grid(row=5, column=0, padx=10, pady=(0, 5), sticky="ew")
+        
+    # When choose "Study type" drop down as CTWA or CT chest + WA, the `optionmenu_rate_formula` will be clickable 
+    def optionmenu_study_type(self, choice):        
+        print("optionmenu_study_type:", study_type_dict[choice])
+        if study_type_dict[choice] in ["whole_abd", "chest_whole_abd"]:
+            self.optionmenu_rate_formula.configure(state = "normal")
+        else:
+            self.optionmenu_rate_formula.configure(state = "disabled")
+            
+            
+    # When click "Rate calculation" drop down as "Choose delayed time", `entry_delay_sec` is enterable 
+    def optionmenu_rate_formula_event(self, choice):
+        print("optionmenu_rate_formula:", rate_formula_state[choice])
+        std_type = self.get_study_type()
+        if rate_formula_state[choice] == "normal" and std_type in ["whole_abd", "chest_whole_abd"]:
+            self.entry_delay_sec.configure(state = "normal")
+        else:
+            self.entry_delay_sec.configure(state = "disabled")
+    
+    # Get "study_type"
+    def get_study_type(self):
+        return study_type_dict[self.optionmenu_study_type.get()]
+    # Get All Input Values
     def get(self):
         out = {
-            "study_type": study_type_dict[self.optionmenu_study_type.get()],
-            "weight_kg": float(self.entry_weight_kg.get()),
+            "study_type": self.get_study_type(),
+            "weight_kg": attempt_float(self.entry_weight_kg.get()),
             "is_first_study": is_first_study_dict[self.optionmenu_is_first_study.get()],
+            "rate_formula": rate_formula_dict[self.optionmenu_rate_formula.get()],
+            "delay_sec": attempt_float(self.entry_delay_sec.get())
         }
         return out
-
 
 class MyOutputFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         # self.grid_rowconfigure(4, weight=1)
         # Output TextBox
-        self.output_textbox = customtkinter.CTkTextbox(self, width=400, height=300)
+        self.output_textbox = customtkinter.CTkTextbox(self, width=400, height=310, font= customtkinter.CTkFont(size=15, weight="normal"))
         self.output_textbox.grid(
             row=0, column=0, padx=(10, 10), pady=(10, 10), sticky="nsew"
         )
@@ -84,10 +128,10 @@ class MyOutputFrame(customtkinter.CTkFrame):
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-
+        
         self.title("Design Pediatric CT")
-        # self.geometry("700x390")
-        self.minsize(700, 390)
+        # self.geometry("742x405") # 700, 390
+        self.minsize(742, 405)
 
         self.grid_columnconfigure((0, 1), weight=1)
         self.grid_rowconfigure((0, 1), weight=2)
--- module.py
+++ module.py
@@ -3,7 +3,7 @@ import customtkinter
 
 
 class MyOptionMenu(customtkinter.CTkFrame):
-    def __init__(self, master, values=["option 1", "option 2"], label_text="Options:"):
+    def __init__(self, master, values=["option 1", "option 2"], label_text="Options:", **kwargs):
         super().__init__(master)
 
         ## Label
@@ -11,7 +11,7 @@ class MyOptionMenu(customtkinter.CTkFrame):
         self.optionmenu_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
         
         ## Drop down
-        self.optionmenu = customtkinter.CTkOptionMenu(self, values=values)
+        self.optionmenu = customtkinter.CTkOptionMenu(self, values=values, **kwargs)
         self.optionmenu.grid(row=0, column=1, padx=5, pady=10, sticky="w")
         
     def set(self, value):
@@ -19,10 +19,13 @@ class MyOptionMenu(customtkinter.CTkFrame):
 
     def get(self):
         return self.optionmenu.get()
+    
+    def configure(self, require_redraw=False, **kwargs):
+        self.optionmenu.configure(require_redraw = require_redraw, **kwargs)
 
 
 class MyEntry(customtkinter.CTkFrame):
-    def __init__(self, master, placeholder_text="CTkEntry", label_text="Entry:"):
+    def __init__(self, master, placeholder_text="CTkEntry", label_text="Entry:", **kwargs):
         super().__init__(master)
 
         ## Label
@@ -30,8 +33,11 @@ class MyEntry(customtkinter.CTkFrame):
         self.entry_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
         
         ## Entry
-        self.entry = customtkinter.CTkEntry(self, placeholder_text=placeholder_text)
+        self.entry = customtkinter.CTkEntry(self, placeholder_text=placeholder_text, **kwargs)
         self.entry.grid(row=0, column=1, padx=5, pady=10, sticky="w")
 
     def get(self):
         return self.entry.get()
+    
+    def configure(self, require_redraw=False, **kwargs):
+        self.entry.configure(require_redraw = require_redraw, **kwargs)
